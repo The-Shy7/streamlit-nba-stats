290098e25b77a283a06e46074cf35d4592bad757
The-Shy7 <tahmintalukder98@gmail.com>
2021-03-01 12:01:33 -0800

    initial commit

---
+++ nba_stats.py
@@ -0,0 +1,11 @@
+# imports
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('NBA Player Stats Exploratory Data Analysis')
+st.markdown("""
+    This app web scrapes NBA player stats data.
+    * **Data source:** [https://www.basketball-reference.com/](https://www.basketball-reference.com/)
+    * **Python libraries:** streamlit, pandas, numpy 
+""")
