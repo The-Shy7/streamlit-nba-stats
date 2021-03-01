2ae43c9ba99ccf9f419e48d628f733376d24ea3c
The-Shy7 <tahmintalukder98@gmail.com>
2021-03-01 12:22:18 -0800

    implemented web scraping function

--- nba_stats.py
+++ nba_stats.py
@@ -9,3 +9,20 @@ st.markdown("""
     * **Data source:** [https://www.basketball-reference.com/](https://www.basketball-reference.com/)
     * **Python libraries:** streamlit, pandas, numpy 
 """)
+
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2020))))
+
+# web scrape nba player stats from basketball-reference
+@st.cache # function will be cached
+def load_data(year):
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    raw = df.drop(df[df.Age == 'Age'].index) # deletes repeating headers in the content
+    raw = raw.fillna(0)
+    playerStats = raw.drop(['Rk'], axis = 1) # redundant with index provided by pandas
+    return playerStats
+
+playerStats = load_data(selected_year)
+
